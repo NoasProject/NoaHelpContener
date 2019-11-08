00c3550fccbac00b1cc3e96420cfe883d594bca6
NoasProject <lowntry@gmail.com>
2019-11-09 01:52:15 +0900

    IPAdressが変更されたら出力する

--- IP-Print.sh
+++ IP-Print.sh
@@ -1,4 +1,19 @@
-while true; 
-    do /sbin/ifconfig en1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/ .*//'; 
-    sleep 2s; 
+#!/bin/sh
+
+IPAdress=""
+PreIPAdress=""
+
+while true; do 
+    PreIPAdress=`/sbin/ifconfig en1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/ .*//'`
+    
+    # IPの変更が発生
+    if [ "$IPAdress" != "$PreIPAdress" ]; then
+        echo "IPアドレスが変更されました${IPAdress} --> ${PreIPAdress}"
+        IPAdress=${PreIPAdress}
+    #else
+        #echo $IPAdress" --- "$PreIPAdress
+    fi
+    sleep 1s
 done;
+
+function GetIP() {}
